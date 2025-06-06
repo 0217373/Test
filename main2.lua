a320064e14fbf9f4ff3b0f54aee4bcf63d224f3b
7373 <moghadami0217373@gmail.com>
2025-06-06 11:51:28 +0400

    main2.lua

--- main2.lua
+++ main2.lua
@@ -1,3 +1,7 @@
 print( type("hello world"))      =  string
 
 print( type(2233))               =  number
+
+print( type(false))              =  boolean
+
+print( type(true))               =  boolean
