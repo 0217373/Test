ef60ccfdd3835eadeb6a8b014db48e1b78f9fb13
7373 <moghadami0217373@gmail.com>
2025-06-06 20:17:07 +0400

    main6.lua

---
+++ main6.lua
@@ -0,0 +1,27 @@
+-- az alamat tajob va soal va alamat dige be ghir az (" _ ") Mamnon mibashad
+
+local user_a = "Navid"
+
+print(user_a)
+
+-- adad az aval nabayad bzrim , dar vasat va akhar moshkeli nadarad
+
+local us2er_2 = "Navid"
+
+print(us2er_2)
+
+-- keyword (and) (Break) (do) (else) (elseif) (end) (false) (until) (while)
+--(for) (function) (if) (in) (local) (nil) (not) (or) (repeat) (return) (then) (true) 
+-- nemitavan estefade kard : )
+
+-- dar estefade az horof bozorg va kochak degat konid
+
+local user = "Navid"
+
+print(user)
+
+-- snake case : in_fore_sale
+
+local in_fore_sale = "7373"
+
+print(in_fore_sale)
